3d1a14878309bd9a271aed6c4b7defbd9be5c0b8
Rémi Defoor <remi.defoor@student.howest.be>
2022-03-26 09:22:38 +0100

    add err handling

--- assets/js/sign-up.js
+++ assets/js/sign-up.js
@@ -3,15 +3,41 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init(evt) {
-    document.querySelector('#sign-up').addEventListener('submit', ensurePwdEquality);
+    document.querySelector('form').addEventListener('submit', createUser);
 }
 
-function ensurePwdEquality(evt) {
-    evt.preventDefault();
+function resetPwdFields() {
+    const errColor = '#ff0000'
+
+    const $pwd = document.querySelector('#password');
+    $pwd.value = '';
+    $pwd.style.borderColor = errColor;
+
+    const $confirmedPwd = document.querySelector('#confirmed-password');
+    $confirmedPwd.value = '';
+    $confirmedPwd.style.borderColor = errColor;
+}
+
+function displayErrs(errs) {
+    const $errs = document.querySelector('#errors');
+    $errs.innerHTML = '';
+
+    errs.forEach(err => {
+        $errs.insertAdjacentHTML('beforeend', `<li>${err}</li>`);
+    });
+
+    resetPwdFields();
+}
+
+function pwdsAreEqual() {
     const $pwd = document.querySelector('#password').value;
     const $confirmedPwd = document.querySelector('#confirmed-password').value;
+    return $pwd === $confirmedPwd;
+}
 
-    if ($pwd === $confirmedPwd) {
-
+function createUser(evt) {
+    evt.preventDefault();
+    if (pwdsAreEqual()) {
+        
     }
 }
