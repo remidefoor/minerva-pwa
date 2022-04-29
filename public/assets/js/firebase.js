4a0d9d7595a40b31cd8e5cc9005efdb15c4beb89
Rémi Defoor <remi.defoor@student.howest.be>
2022-04-29 16:33:08 +0200

    deploy on firebase

---
+++ public/assets/js/firebase.js
@@ -0,0 +1,19 @@
+  'use strict';
+  
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDrD0F1dIxoCvVKIQG1jRRM7px9zvtQXd4",
+    authDomain: "web-and-mobile-technolog-bf41b.firebaseapp.com",
+    projectId: "web-and-mobile-technolog-bf41b",
+    storageBucket: "web-and-mobile-technolog-bf41b.appspot.com",
+    messagingSenderId: "558455484498",
+    appId: "1:558455484498:web:c507b1cf1d7098937aaf8b"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
